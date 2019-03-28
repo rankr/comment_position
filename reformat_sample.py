8170661d1a33cfccc07db417fb74637622a61de5
Hao He <hehao1998@pku.edu.cn>
2019-03-28 20:59:18 +0800

    Modified directories and reformatted some code

--- reformat_sample.py
+++ reformat_sample.py
@@ -1,32 +1,34 @@
 # This piece of code aim to transfer content in sample.csv
 # to markdown format so that it is more reader friendly
 
-import csv;
+import csv
 
 if __name__ == '__main__':
-    with open('sample.md', 'w') as mdfile:
+    with open('result/sample.md', 'w') as mdfile:
         mdfile.write('# Samples\n\n')
 
-        with open('sample.csv') as csvfile:
+        with open('result/sample.csv') as csvfile:
             samples = csv.reader(csvfile)
 
             sample_num = 1
             for sample_row in samples:
-                mdfile.write('## ' + str(sample_num) + '. Sample ' + sample_row[0] + '\n\n')
+                mdfile.write('## ' + str(sample_num) +
+                             '. Sample ' + sample_row[0] + '\n\n')
 
                 mdfile.write('File Path: ' + sample_row[1] + '\n\n')
                 mdfile.write('Comment Line Number: ' + sample_row[3] + '\n\n')
                 mdfile.write('Label: ' + sample_row[4] + '\n\n')
 
                 mdfile.write('```c++\n')
-                comment = sample_row[2].replace('[comma]', ',').replace('[enter]', '\n')
+                comment = sample_row[2].replace(
+                    '[comma]', ',').replace('[enter]', '\n')
                 mdfile.write(comment)
                 mdfile.write('\n```\n\n')
 
                 mdfile.write('```c++\n')
-                code = sample_row[5].replace('[comma]', ',').replace('[enter]', '\n')
+                code = sample_row[5].replace(
+                    '[comma]', ',').replace('[enter]', '\n')
                 mdfile.write(code)
                 mdfile.write('\n```\n\n')
 
                 sample_num += 1
-
--- sample.py
+++ sample.py
@@ -3,31 +3,30 @@
 import random as rd
 import pandas as pd
 
-#sample from 49836-1 commment-code pairs
+# sample from 49836-1 commment-code pairs
 
 
 s = set()
 i = 0
 while 1:
-	k = rd.randint(2, 49836)
-	if k not in s:
-		s.add(k)
-		i += 1
-		if i == 300:
-			break
+    k = rd.randint(2, 49836)
+    if k not in s:
+        s.add(k)
+        i += 1
+        if i == 300:
+            break
 a = list(s)
 a.sort()
 with open('result/sample.txt', 'w') as w:
-	for i in a:
-		w.write("%d\n"%i)
+    for i in a:
+        w.write("%d\n" % i)
 
 
-
-a = pd.read_csv('comment_position.csv')
+a = pd.read_csv('result/comment_position.csv')
 res = []
 with open('result/sample.txt') as f:
     d = f.readlines()
     d = [int(i.strip()) for i in d]
     res = a.loc[d]
 
-res.to_csv('sample.csv')
+res.to_csv('result/sample.csv')
