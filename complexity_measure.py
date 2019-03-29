14ae5b916598892a4c24655a7b9703d323b20c6f
Yixuan Guo <2761683494@qq.com>
2019-03-29 23:27:31 +0800

    alter the code structure of complexity_measure.py
    update the module measuring the number of variables

--- complexity_measure.py
+++ complexity_measure.py
@@ -7,13 +7,14 @@ Created on Thu Mar 28 13:20:13 2019
 
 import re
 
-#统计代码块的复杂度，度量指标有代码行数、大括号最深层数、变量个数、运算符密集程度
-def complexity_measure(string):
-    #行数:line_num
+#获取代码块行数，被complexity_measure(string)调用
+def get_line_num(string):
     line_num = len(re.findall('\n',string))
     print("line number is :{}".format(line_num))
-    
-    #大括号的最大层数:max_depth
+    return line_num
+
+#获取代码块最大深度，进入一个大括号深度就加1，被complexity_measure(string)调用
+def get_max_depth(string):
     max_depth = 0
     count_depth = 0
     for each_char in string:
@@ -24,22 +25,64 @@ def complexity_measure(string):
         if count_depth > max_depth:
             max_depth = count_depth
     print("max bracket depth is :{}".format(max_depth))
+    return max_depth
+
+#预处理函数将代码段按行切片,用666替换注释里的'//' '/*' '*/' '///' 'for' 'do' 'while'
+#返回处理后的list
+def preprocess(string): 
+    lines = re.split('\n',string)
+    for i in range(len(lines)):
+        if('/*' in lines[i]):
+            lines[i]=lines[i].replace("/*","666")
+        if('*/' in lines[i]):
+            lines[i]=lines[i].replace("*/","666")
+        if('//' in lines[i]):
+            lines[i]=lines[i].replace("//","666")
+        if('///' in lines[i]):
+            lines[i]=lines[i].replace("///","666")
+        if('||' in lines[i]):
+            lines[i]=lines[i].replace("||","666")
+        if('&&' in lines[i]):
+            lines[i]=lines[i].replace("&&","666")
+    return lines
+
+def get_variable_num(string):
+    lines = preprocess(string)
+    key_word_list = ['void',  'auto',  'short',  'long',   'int','float','double',  'char', 'struct',
+                    'class',   'if' ,  'else' ,'switch',  'case',  'for',    'do', 'while', 'return'
+                     'goto','continue','break', 'const', 'sizeof', 'return', 'typdef', 'extern','enum',
+                     'register', 'default','union', 'static', 'volatile']
+    variable_num = 0
+    var_name_pattern = re.compile("\w+")
     
-    #变量个数，检测在此模块定义的变量的个数，包括指针，
-    #有个疑问是如何加入检测自定义的struct变量，这个有点困难
-    int_pattern = re.compile("int")
-    float_pattern = re.compile("float")
-    double_pattern = re.compile("double")
-    struct_pattern = re.compile("struct")
-    
-    int_num = len(int_pattern.findall(string))
-    float_num = len(float_pattern.findall(string))
-    double_num = len(double_pattern.findall(string))
-    struct_num = len(struct_pattern.findall(string))
-    variable_total_num = int_num + float_num + double_num + struct_num
-    print("basic variable number is:", variable_total_num)
-    
-    #运算符密集度(算术运算，位运算，单位：个/行)
+    variable_list = []
+    for i in range(len(lines)):
+        for each_key_word in key_word_list:
+            #去掉行中出现的关键词
+            if each_key_word in lines[i]:
+                lines[i] = lines[i].replace(each_key_word, "")
+        #把所有的变量名（包括函数名）放在同一列表variable_list
+        variable_list += var_name_pattern.findall(lines[i])
+    #去掉variable_list中重复元素
+    no_repeat_list = list(set(variable_list))
+    #print(no_repeat_list)
+
+    #把数字开头的元素从no_repeat_list列表中去掉
+    for i in range(len(no_repeat_list)):
+        r = re.match('^\d',no_repeat_list[i])
+        #r为none时，说明出现数字开头
+        if r:
+            no_repeat_list[i] = ""    
+    #删除空字符串
+    no_repeat_list.remove("")
+    #print(no_repeat_list)
+    variable_num = len(no_repeat_list)
+    return variable_num
+
+#获得运算符数目
+def get_operator_num(string):
+    code_line_array = preprocess(string)
+    #运算符考虑(算术运算+-*/%，位运算|&)
     plus_pattern = re.compile("\+")
     plus_plus_pattern = re.compile("\+\+")
     product_pattern = re.compile("\*")
@@ -55,21 +98,7 @@ def complexity_measure(string):
     minus_num = len(re.findall('-',string))
     minus_minus_num = len(re.findall('--',string))
     minus_num = minus_num - minus_minus_num*2
-    #统计乘号和除号前现将代码段按行切片,先预处理用666替换注释里的'//'和'/*'和'*/'还有'///'
-    code_line_array = re.split('\n',string)
-    for each_line in code_line_array:
-        if('/*' in each_line):
-            each_line.replace("/*","666")
-        if('*/' in each_line):
-            each_line.replace("*/","666")
-        if('//' in each_line):
-            each_line.replace("//","666")
-        if('///' in each_line):
-            each_line.replace("///","666")
-        if('||' in each_line):
-            each_line.replace("||","666")
-        if('&&' in each_line):
-            each_line.replace("&&","666")
+    
     #因为乘除法运算一般伴随着赋值操作，所以检测和'='同一行出现的乘号和除号的数量，
     #但是这样做无法取出指针*带来的误差
     #(3)运算符'*'号数量，
@@ -102,6 +131,22 @@ def complexity_measure(string):
 #    print("|:",bit_or_num)
 #    print("&:",bit_and_num)
     print("total operator number is:",operator_total_num)
+    return operator_total_num
+
+#统计代码块的复杂度，度量指标有代码行数、大括号最深层数、变量个数、运算符密集程度
+def complexity_measure(string):
+    #行数:line_num
+    line_num = get_line_num(string)
+    
+    #大括号的最大层数:max_depth
+    max_depth = get_max_depth(string)
+    
+    #变量个数，检测在此模块定义的变量的个数，包括指针，
+    variable_total_num = get_variable_num(string)
+    
+    #运算符密集度(算术运算，位运算，单位：个/行)
+    operator_total_num = get_operator_num(string)
+    
     op_line_rate = operator_total_num/line_num
     print("operator density is:%.2f each line"%(op_line_rate))
     return [line_num, max_depth, variable_total_num, operator_total_num, op_line_rate]
